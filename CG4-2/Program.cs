9ffa9cf4054fafbc596772369c046c56cbcc52b6
KendallU <kendallunion@gmail.com>
2018-08-04 22:19:22 -0500

    Add project files.

---
+++ CG4-2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace CG4_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] intArray = new int[6] { 1, 1, 2, 3, 5, 8 };
+            intArray[0] = 1;
+            intArray[1] = 1;
+            intArray[2] = 2;
+            intArray[3] = 3;
+            intArray[4] = 5;
+            intArray[5] = 8;
+            
+            for (int i = 0; i < intArray.Length; i++)
+            {
+                Console.WriteLine(intArray[i]);
+            }
+            Console.ReadLine();
+                
+        }
+    }
+}
